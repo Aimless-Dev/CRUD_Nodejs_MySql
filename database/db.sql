7a571c4c6b4af0ac20f48e8567068817d5355f78
Aimless <gufillex122@gmail.com>
2021-07-22 16:36:51 -0500

    Carpetas

---
+++ database/db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE crudnodejs;
+
+USE crudnodejs;
+
+CREATE TABLE customer (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone VARCHAR(15)
+);
+
+SHOW TABLES;
+DESCRIBE customer;
